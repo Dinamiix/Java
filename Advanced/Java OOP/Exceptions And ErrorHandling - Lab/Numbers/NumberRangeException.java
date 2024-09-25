e100c53afa430d27a4a8cd0e9efb06c0dad7aab4
Iliyan Karanikolov <dinamix732@gmail.com>
2024-09-25 17:16:48 +0300

    Add files via upload

---
+++ Advanced/Java OOP/Exceptions And ErrorHandling - Lab/Numbers/NumberRangeException.java
@@ -0,0 +1,11 @@
+package Numbers;
+
+public class NumberRangeException extends RuntimeException {
+    public NumberRangeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NumberRangeException(String message) {
+        super(message);
+    }
+}
