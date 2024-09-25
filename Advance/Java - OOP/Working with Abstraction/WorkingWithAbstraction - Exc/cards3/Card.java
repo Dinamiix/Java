8f119772e7465f71e625b5b0049bbac733493a67
Iliyan Karanikolov <dinamix732@gmail.com>
2024-09-25 17:37:38 +0300

    Add files via upload

---
+++ Advance/Java - OOP/Working with Abstraction/WorkingWithAbstraction - Exc/cards3/Card.java
@@ -0,0 +1,13 @@
+public class Card {
+
+    // силата на картата = силата на боята + силата на ранка
+    private int power;
+
+    public Card(int power) {
+        this.power = power;
+    }
+
+    public int getPower() {
+        return this.power;
+    }
+}
---
+++ Advance/Java - OOP/Working with Abstraction/WorkingWithAbstraction - Exc/trafficLights/Light.java
@@ -0,0 +1,6 @@
+public enum Light {
+
+    RED,
+    GREEN,
+    YELLOW;
+}
