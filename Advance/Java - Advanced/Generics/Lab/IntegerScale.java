2acc0acb990afb7aaf723b35b4aba1031c7afa23
Iliyan Karanikolov <dinamix732@gmail.com>
2024-09-25 17:46:29 +0300

    Add files via upload

---
+++ Advance/Java - Advanced/Generics/Lab/IntegerScale.java
@@ -0,0 +1,25 @@
+package Lab;
+
+public class IntegerScale {
+
+    private Integer left;
+    private Integer right;
+
+    public IntegerScale(Integer left, Integer right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public Integer getHeavier() {
+        int result = left.compareTo(right);
+
+        if (result > 0) {
+            return left;
+        } else if (result < 0 ) {
+            return right;
+        }
+
+        return left;
+    }
+
+}
---
+++ Advance/Java - Advanced/Generics/Lab/Printer.java
@@ -0,0 +1,16 @@
+package Lab;
+
+public class Printer<T> {
+
+    private T toPrint;
+
+    public Printer(T toPrint) {
+        this.toPrint = toPrint;
+    }
+
+    public T print() {
+        System.out.println(toPrint);
+        return toPrint;
+    }
+
+}
