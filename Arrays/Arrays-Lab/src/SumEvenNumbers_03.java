fdc54a06966ea8c80fbbccd453d31171b2befc1c
Iliyan Karanikolov <dinamix732@gmail.com>
2024-09-26 18:35:54 +0300

    Add files via upload

---
+++ Arrays/Arrays-Lab/src/SumEvenNumbers_03.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumEvenNumbers_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int sumEven = 0;
+
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                sumEven += number;
+            }
+        }
+        System.out.println(sumEven);
+    }
+}
