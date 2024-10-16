28403077172ec9228ef67860ef2c4cf83b7a0b2a
Iliyan Karanikolov <dinamix732@gmail.com>
2024-10-16 21:27:16 +0300

    Delete Generics directory

--- Generics/Lecture/GenericArrayCreator/Main.java
+++
@@ -1,9 +0,0 @@
-package GenericArrayCreator;
-
-public class Main {
-    public static void main ( String[] args ) {
-        String[] strings = ArrayCreator.create ( 10,"none" );
-        Integer[] ints = ArrayCreator.create ( Integer.class, 10,0 );
-        System.out.println ( );
-    }
-}
